2f7c178b9d55066f2f104ae7965dc086e2cac4a2
ThengdeNitin <nitinthengde14368@gmail.com>
2025-09-28 19:26:18 +0530

    updated server and vercel

--- backend/server.js
+++ backend/server.js
@@ -15,27 +15,27 @@ const PORT = process.env.PORT || 4000;
 
 connectDB();
 
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], 
-  credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: process.env.VITE_FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use('/api/user', userRouter);
-app.use('/api/posts', postRouter); 
-
+app.use('/api/posts', postRouter);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ success: false, message: 'Internal Server Error', error: err.message });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
