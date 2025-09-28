6b90d781b72c478e580c76decc0f9fe086550a59
ThengdeNitin <nitinthengde14368@gmail.com>
2025-09-28 19:36:28 +0530

    update multer

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,22 +1,24 @@
 import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary-v2';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import { v2 as cloudinary } from 'cloudinary';
 
-
+// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_SECRET_KEY,
 });
 
+// Setup Cloudinary storage
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary,
   params: {
-    folder: 'social_media_app', 
-    allowed_formats: ['jpg', 'jpeg', 'png', 'mp4', 'mov'], 
+    folder: 'social_media_app', // Cloudinary folder
+    allowed_formats: ['jpg', 'jpeg', 'png', 'mp4', 'mov'], // allowed file types
   },
 });
 
+// Multer middleware
 const upload = multer({ storage });
 
 export default upload;
