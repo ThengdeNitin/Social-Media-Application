8e02336d140ebaf7bcaba16ecdb8475900c94926
ThengdeNitin <nitinthengde14368@gmail.com>
2025-09-30 12:44:47 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         <Routes>
           <Route path ='/' element = {<Landing/>}/>
           <Route path = '/register' element = {<Register/>}/>
-          <Route path = '//forgot-password' element = {<ForgotPassword/>}/>
+          <Route path = '/forgot-password' element = {<ForgotPassword/>}/>
         </Routes>
       )}
     </div>
